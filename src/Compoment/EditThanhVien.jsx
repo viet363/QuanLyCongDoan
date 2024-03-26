b183b48ac951a4a6977f5f644bc11a1f36f6ad59
Lê Quốc Việt <lequocviet362003@gmail.com>
2024-03-26 20:43:42 +0700

    26/3

--- src/Compoment/EditThanhVien.jsx
+++ src/Compoment/EditThanhVien.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function EditThanhVien() {
   return (
-    <div>EditThanhVien</div>
+    <div>
+    <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+      <div>
+        <p>Sửa thành viên</p>
+      </div>
+    </div>
+    <div className='flex flex-col items-center justify-center w-full h-[1000px] bg-[#EEEEEE]'>
+      <div className="flex flex-col items-start">
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="text" placeholder='Mã' />
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="text" placeholder='Tên' />
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="number" placeholder='Số điện thoại' />
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="text" placeholder='Địa chỉ' />
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="email" placeholder='Email' />
+        <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="text" placeholder='Chức vụ' />
+      </div>
+      <div class=' flex w-[223px] h-[80px] bg-[#515151] mt-7 rounded-3xl'>
+        <button class='text-white'>Xác Nhận</button>
+      </div>
+    </div>
+  </div>
   )
 }
