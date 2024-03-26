2b0a326b605dfdd618e183067f485feeb7b608ae
Lê Quốc Việt <lequocviet362003@gmail.com>
2024-03-26 21:36:24 +0700

    26/3

--- src/Compoment/EditThanhVien.jsx
+++ src/Compoment/EditThanhVien.jsx
@@ -17,7 +17,7 @@ export default function EditThanhVien() {
         <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="email" placeholder='Email' />
         <input className='w-[940px] h-[80px] outline-none bg-[#ffffff] mt-7 rounded-3xl' type="text" placeholder='Chức vụ' />
       </div>
-      <div class=' flex w-[223px] h-[80px] bg-[#515151] mt-7 rounded-3xl'>
+      <div class=' flex w-[223px] h-[80px] bg-[#515151] mt-7 rounded-3xl justify-center items-center'>
         <button class='text-white'>Xác Nhận</button>
       </div>
     </div>
